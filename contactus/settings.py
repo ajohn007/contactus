796f707f61c5a1689f216022f580c963cffea7ec
alphin <alphin@qburst.com>
2020-10-12 07:07:17 -0400

    Contactus Database created

--- contactus/settings.py
+++ contactus/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    rest_framework'
+    'countries.apps.CountriesConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
