76e36800864e5d4df7efd725de6d7d87cc1fa432
alphin <alphin@qburst.com>
2020-10-13 02:15:53 -0400

    Modified Commit

--- contactus/settings.py
+++ contactus/settings.py
@@ -37,13 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    rest_framework'
-    'countries.apps.CountriesConfig',
+    'rest_framework',
+    'contactusappli.apps.ContactusappliConfig',
     'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'contactus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'contactusdb',
+        'USER': 'postgres',
+        'PASSWORD': 'nellikkattil',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
