567642b5f2ab429fc57e1621a5ad38a54dbbcdb3
alphin <alphin@qburst.com>
2020-10-13 07:18:01 -0400

    Admin created

--- contactusappli/models.py
+++ contactusappli/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    number=models.DecimalField(max_digits=10, decimal_places=0)
+    subject=models.TextField()
+   
+    def __str__(self):
+        return self.name
