774f9bf67191adeb53d1dc16895d175f58d54801
alphin <alphin@qburst.com>
2020-10-14 08:34:27 -0400

    procfile committed

--- contactus/settings.py
+++ contactus/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '09qm9o$+!+#j01f1+1ta%v-r-+8by&u*$gdk48lla!!=-(=kv%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','contactusappqburst.herokuapp.com/']
 
