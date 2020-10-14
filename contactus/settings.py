0db20850118ddfeb78636d42ff9f86aee4e09c3c
alphin <alphin@qburst.com>
2020-10-14 07:37:54 -0400

    settings committed

--- contactus/settings.py
+++ contactus/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '09qm9o$+!+#j01f1+1ta%v-r-+8by&u*$gdk48lla!!=-(=kv%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','contactusappqburst.herokuapp.com/']
 
 
 # Application definition
